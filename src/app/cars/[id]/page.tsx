4b0d231aea37f03278861e1eb4efaa8657bc36fb
Adam Palmer <ad@palmerama.tv>
2023-11-05 17:22:37 +0000

    adds all details on car page

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { Car } from "@/app/lib/definitions"
 import { fetchAllCars, fetchCarById } from "@/app/lib/data"
+import commaNumber from 'comma-number'
+import Image from "next/image"
 
 export default async function Page({ params }: { params: { id: string } }) {
   const car = await fetchCarById(params.id)
@@ -10,9 +12,17 @@ export default async function Page({ params }: { params: { id: string } }) {
       {
         car && (
           <div key={car.id}>
-            <h1>{car.manufacturer} {car.model}</h1>
+            <h1>{car.manufacturer} {car.model} {car.trim}</h1>
             <p>{car.year} ({car.regYear}reg)</p>
-            <p></p>
+            <Image
+              src={`/cars/${car.image}`}
+              alt={`${car.year} ${car.manufacturer} ${car.model} ${car.trim}`}
+              width={400}
+              height={400}
+            />
+            <p>{car.fuelType}</p>
+            {car.engineSize && <p>{car.engineSize}</p>}
+            <p>{commaNumber(car.mileage)} miles</p>            
           </div>
         )
       }
