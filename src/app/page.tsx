50aa829ca20ea2f8b8dde804f722f7d21eb441e6
Adam Palmer <ad@palmerama.tv>
2023-11-06 11:26:01 +0000

    mobile styling fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="grid w-full h-[32rem] justify-center items-center text-4xl">
+    <main className="grid w-full h-[32rem] justify-center items-center text-4xl p-7 text-center">
       <div>Today, I want to <Link href="/cars" className="inline-block text-yellow-600 underline underline-offset-7 hover:no-underline">manage my inventory</Link></div>
     </main>
   )
